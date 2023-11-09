e58d3fa4e1598046e9d571500cf6e6e2833d5154
neeeal <matira.nealbartonjames@gmail.com>
2023-11-09 21:04:32 +0800

    update previous flow from directory

--- trainValSplitter.py
+++ trainValSplitter.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import numpy as np
 
-preprocessingPath="stitch-rgb"
+preprocessingPath="close_seg"
 path = "data"
 classes = os.listdir(preprocessingPath)
 trainPath = os.path.join(path,"train")
@@ -10,27 +10,30 @@ valPath = os.path.join(path,"val")
 train = 0.80 # ;val = 1. - train
 np.random.seed(26)
 
-if os.path.exists(path): shutil.rmtree(path); os.mkdir(path)
-else: os.mkdir(path)
+with open("data.csv","w") as f:
+    f.write("path,ph")
 
-for s in [trainPath, valPath]:
-    if os.path.exists(s): shutil.rmtree(s); os.mkdir(s)
-    else: os.mkdir(s)
+    if os.path.exists(path): shutil.rmtree(path); os.mkdir(path)
+    else: os.mkdir(path)
+
+    for s in [trainPath, valPath]:
+        if os.path.exists(s): shutil.rmtree(s); os.mkdir(s)
+        else: os.mkdir(s)
+        for c in classes:
+            classPath = os.path.join(s,c)
+            if os.path.exists(classPath): shutil.rmtree(classPath); os.mkdir(classPath)
+            else: os.mkdir(classPath)
+
+    ## SEPARATING TO TRAIN AND VALIDATION SETS
     for c in classes:
-        classPath = os.path.join(s,c)
-        if os.path.exists(classPath): shutil.rmtree(classPath); os.mkdir(classPath)
-        else: os.mkdir(classPath)
-
-## SEPARATING TO TRAIN AND VALIDATION SETS
-for c in classes:
-    classPath = os.path.join(preprocessingPath,c)
-    images = np.array(os.listdir(classPath))
-    np.random.shuffle(images)
-    numImages = len(images)
-    setClassPath = os.path.join(trainPath,c)
-    for n,image in enumerate(images):
-        if n == round(numImages*train): 
-            setClassPath = os.path.join(valPath,c)
-        imagePath = os.path.join(classPath,image)
-        newImagePath = os.path.join(setClassPath,image)
-        shutil.copy(imagePath, newImagePath)
+        classPath = os.path.join(preprocessingPath,c)
+        images = np.array(os.listdir(classPath))
+        np.random.shuffle(images)
+        numImages = len(images)
+        setClassPath = os.path.join(trainPath,c)
+        for n,image in enumerate(images):
+            if n == round(numImages*train): 
+                setClassPath = os.path.join(valPath,c)
+            imagePath = os.path.join(classPath,image)
+            newImagePath = os.path.join(setClassPath,image)
+            shutil.copy(imagePath, newImagePath)
